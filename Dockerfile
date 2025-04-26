214b382b0afe756d75a64c8b7901d51aea6f1b13
Alexey Oshev <alexeyosh87@gmail.com>
2025-04-27 00:41:45 +0500

    New version

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM debian:latest
 
 RUN echo "deb http://deb.debian.org/debian/ stable main contrib" > \
-	/etc/apt/sources.list && rm /etc/apt/sources.list.d/debian.sources
+	/etc/apt/sources.list && \
+	echo "deb-src http://deb.debian.org/debian/ stable main contrib" >> \
+	/etc/apt/sources.list && \
+	rm /etc/apt/sources.list.d/debian.sources
 
-RUN apt update && apt install -y git-buildpackage
+RUN apt update && apt install -y dpkg-dev git-buildpackage
+
+RUN mkdir /packages
--- build.sh
+++ build.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
+set -x
+# Autobuilder deb packages with gbp.
 
-docker build . --tag=debuilder:v1
+#-------------------------------------------------------------------------------
+# Variables.
+
+id_docker_image="$(docker images -q debuilder:v1)"
+
+#-------------------------------------------------------------------------------
+# Checking the parameters.
+
+if [ $# -le 0 ]; then
+	echo "Enter a name software for build deb package"
+	exit 1
+fi
+
+#-------------------------------------------------------------------------------
+# Checking the built docker image or building it.
+
+if [ -z "$id_docker_image" ]; then
+	docker build . --tag=debuilder:v1
+fi
+
+#-------------------------------------------------------------------------------
+# Starting a container.
+
+id_docker_image="$(docker images -q debuilder:v1)"
+
+docker container run \
+	--rm \
+	--name="build-$1" \
+	-v "$(pwd)/packages":/packages \
+	"$id_docker_image" \
+	/bin/bash -c \
+	"cd packages && \
+	mkdir $1 && \
+	cd $1 && \
+	apt source $1 && \
+	cd $(find . -type d -name '$1*' | head -n1) && \
+	apt-get build-dep -y $1 && \
+	debuild"
+
+#-------------------------------------------------------------------------------
